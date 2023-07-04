5fae64b1dd24210eda8a85c8f08b500a14cc5d95
Olfa Arfani <olfa.arfani@ynpact.com>
2023-07-04 15:34:52 +0200

    update on groovy

--- maven-project/pipelineScript.groovy
+++ maven-project/pipelineScript.groovy
@@ -17,6 +17,7 @@ pipeline {
 
         stage ('Build') {
             steps {
+                cd maven-project
 		sh 'mvn -Dmaven.test.failure.ignore=true install' 
             }
             post {
