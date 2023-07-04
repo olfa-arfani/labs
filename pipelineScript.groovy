9b10bbd0549db73a679571971fad603bb02f46fd
Olfa Arfani <olfa.arfani@ynpact.com>
2023-07-04 14:40:31 +0200

    add pipeline script groovy

---
+++ pipelineScript.groovy
@@ -0,0 +1,29 @@
+
+pipeline {
+    agent any
+    tools {
+        maven 'Maven 3.9.3'
+        jdk 'jdk11'
+    }
+    stages {
+        stage ('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+            }
+        }
+
+        stage ('Build') {
+            steps {
+                sh 'mvn -Dmaven.test.failure.ignore=true install' 
+            }
+            post {
+                success {
+                    junit 'maven-project/target/surefire-reports/**/*.xml' 
+                }
+            }
+        }
+    }
+}
